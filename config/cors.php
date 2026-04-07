33d0c39009b7c1465da51ec7a6bc37b03e8a11e2
mgmtafhomebiz-hash <mgmt.afhomebiz@gmail.com>
2026-04-07 08:36:01 +0800

    updated cors origin

--- config/cors.php
+++ config/cors.php
@@ -10,16 +10,29 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
+    'allowed_methods' => ['*'],
+
     'allowed_origins' => [
         env('FRONTEND_URL', 'http://localhost:3000'),
         'https://www.afhome.ph',
         'https://afhome.ph',
+        'https://frontend.afhome.ph',
+        'https://backend.afhome.ph',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
-],
-'allowed_origins_patterns' => [
-    '#^https://.*\.vercel\.app$#',
-],
-'supports_credentials' => true,
+    ],
+
+    'allowed_origins_patterns' => [
+        '#^https://.*\\.vercel\\.app$#',
+        '#^https://([a-z0-9-]+\\.)?afhome\\.ph$#',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
 
 ];
