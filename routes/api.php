042bde69c2353605a23cdaa3783cd3d6d0a9e2c3
JRSprog <jsalabao024@gmail.com>
2026-04-29 16:43:39 +0800

    added live viewer

--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,7 @@ use App\Http\Controllers\Api\MemberActivityLogController;
 use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\ShippingRateController;
 use App\Http\Controllers\Api\PasskeyAuthController;
+use App\Http\Controllers\Api\ProductViewerController;
 
 
 // Public auth routes
@@ -124,6 +125,7 @@ Route::middleware('throttle:public')->group(function () {
     Route::get('/address/provinces', [AddressController::class, 'provinces']);
     Route::get('/address/cities', [AddressController::class, 'cities']);
     Route::get('/address/barangays', [AddressController::class, 'barangays']);
+    Route::post('/products/{id}/viewers/heartbeat', [ProductViewerController::class, 'heartbeat']);
 });
 
 
